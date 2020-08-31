fccfa1511a061ecff18cc0365e3729cac70d3a8c
Stepan Kadochnikov <s.kadochnikov@zenit.ru>
2020-08-31 19:05:41 +0300

    Change version to `0.2`

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="zenitai",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     descrption="A collection of tools and utils for data analysis. Contains functions and tools for WOE-transformations and other utils",
 )
+
