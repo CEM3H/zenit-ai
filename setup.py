53d750804e16b8bfd448f8a3d58845110af519d3
Stepan Kadochnikov <s.kadochnikov@zenit.ru>
2020-09-02 13:57:28 +0300

    Change package version to 0.3, update docs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ZenitAI",
-    version="0.3a",
+    version="0.3",
     packages=find_packages(),
     descrption="A collection of tools and utils for data analysis. Contains functions and tools for WOE-transformations and other utils",
 )
