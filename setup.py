cb462002be0c08d485b8152e068cf1f219e10ee1
Stepan Kadochnikov <s.kadochnikov@zenit.ru>
2020-09-17 12:49:46 +0300

    Build 0.3.1
    Fix in docstrings, fix in monotonic borders

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ZenitAI",
-    version="0.3",
+    version="0.3.1",
     packages=find_packages(),
     descrption="A collection of tools and utils for data analysis. Contains functions and tools for WOE-transformations and other utils",
 )
