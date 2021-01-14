35ed2405d13a21576bf41d582104748df6755ad9
kadochnikov.sv <kadochnikov.sv@gmail.com>
2021-01-14 20:05:23 +0300

    version 0.4.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ZenitAI",
-    version="0.4.1",
+    version="0.4.2",
     packages=find_packages(),
     descrption="""
     A collection of tools and utils for data analysis.
