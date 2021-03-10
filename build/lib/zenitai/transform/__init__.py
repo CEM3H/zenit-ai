8e278e832a48f7cb49016abbe647ed85e959bb03
Stepan Kadochnikov <s.kadochnikov@zenit.ru>
2021-03-10 10:21:57 +0300

    add some docstrings

---
+++ build/lib/zenitai/transform/__init__.py
@@ -0,0 +1,26 @@
+"""
+__init__
+"""
+
+
+from ._woe import (
+    WoeTransformer,
+    WoeTransformerRegularized,
+    group_plot,
+    grouping,
+    monotonic_borders,
+    statistic,
+    woe_apply,
+    woe_transformer,
+)
+
+__all__ = [
+    "WoeTransformer",
+    "WoeTransformerRegularized",
+    "woe_transformer",
+    "woe_apply",
+    "group_plot",
+    "grouping",
+    "statistic",
+    "monotonic_borders",
+]
