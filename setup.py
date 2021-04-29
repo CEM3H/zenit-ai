7dcd45d01061d5bbb71372fbb05016ff7b7396d3
kadochnikov.sv <kadochnikov.sv@gmail.com>
2021-04-29 10:08:57 +0300

    remove sphinx-related rows from setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
-from sphinx.setup_command import BuildDoc
 
-cmdclass = {"build_sphinx": BuildDoc}
+# from sphinx.setup_command import BuildDoc
+
+# cmdclass = {"build_sphinx": BuildDoc}
 setuptools.setup()
